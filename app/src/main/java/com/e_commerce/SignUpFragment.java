71a07d88e9c5c383aeae562b006b04cb3cbdcb77
Mateusz <mateuszd2411@gmail.com>
2020-03-28 10:10:46 +0100

    I added ratings function (+firebase) when you register new account

--- app/src/main/java/com/e_commerce/SignUpFragment.java
+++ app/src/main/java/com/e_commerce/SignUpFragment.java
@@ -29,10 +29,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -268,23 +271,50 @@ public class SignUpFragment extends Fragment {
                                                 @Override
                                                 public void onComplete(@NonNull Task<Void> task) {
                                                     if (task.isSuccessful()){
-                                                        Map<String,Object> listSize = new HashMap<>();
-                                                        listSize.put("list_size",(long) 0);
-                                                        firebaseFirestore.collection("USERS").document(firebaseAuth.getUid()).collection("USER_DATA").document("MY_WISHLIST")
-                                                            .set(listSize).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                            @Override
-                                                            public void onComplete(@NonNull Task<Void> task) {
-                                                                if (task.isSuccessful()){
-                                                                    mainIntent();
-                                                                }else {
-                                                                    progressBar.setVisibility(View.INVISIBLE);
-                                                                    signUpBtn.setEnabled(true);
-                                                                    signUpBtn.setTextColor(Color.rgb(255,255,255));
-                                                                    String error = task.getException().getMessage();
-                                                                    Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+
+                                                        CollectionReference userDataReference = firebaseFirestore.collection("USERS").document(firebaseAuth.getUid()).collection("USER_DATA");
+
+                                                        ////maps
+
+                                                        Map<String,Object> wishlistMap = new HashMap<>();
+                                                        wishlistMap.put("list_size",(long) 0);
+
+                                                        Map<String,Object> ratingsMap = new HashMap<>();
+                                                        ratingsMap.put("list_size",(long) 0);
+
+                                                        ///maps
+
+                                                        final List<String> documentNames = new ArrayList<>();
+                                                        documentNames.add("MY_WISHLIST");
+                                                        documentNames.add("MY_RATINGS");
+
+                                                        List<Map<String,Object>> documentFields = new ArrayList<>();
+                                                        documentFields.add(wishlistMap);
+                                                        documentFields.add(ratingsMap);
+
+                                                        for (int x = 0; x < documentNames.size();x++){
+
+                                                            final int finalX = x;
+                                                            userDataReference.document(documentNames.get(x))
+                                                                    .set(documentFields.get(x)).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                    if (task.isSuccessful()){
+                                                                        if (finalX == documentNames.size() -1){
+                                                                            mainIntent();
+                                                                        }
+                                                                    }else {
+                                                                        progressBar.setVisibility(View.INVISIBLE);
+                                                                        signUpBtn.setEnabled(true);
+                                                                        signUpBtn.setTextColor(Color.rgb(255,255,255));
+                                                                        String error = task.getException().getMessage();
+                                                                        Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                                                                    }
                                                                 }
-                                                            }
-                                                        });
+                                                            });
+
+                                                        }
+
                                                     }
                                                     else {
                                                         String error = task.getException().getMessage();
