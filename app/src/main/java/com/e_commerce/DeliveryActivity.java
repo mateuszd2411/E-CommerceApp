a7eb7e983b5650f091ccd9210bf600ac3a854de7
Mateusz <mateuszd2411@gmail.com>
2020-04-13 14:37:12 +0200

    I added payment method paytm (bug with delivery system)

--- app/src/main/java/com/e_commerce/DeliveryActivity.java
+++ app/src/main/java/com/e_commerce/DeliveryActivity.java
@@ -4,19 +4,45 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+import com.paytm.pgsdk.PaytmOrder;
+import com.paytm.pgsdk.PaytmPGService;
+import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class DeliveryActivity extends AppCompatActivity {
 
@@ -29,7 +55,11 @@ public class DeliveryActivity extends AppCompatActivity {
     private TextView fullAddress;
     private TextView pincode;
     private Button continueBtn;
+    private Dialog loadingDialog;
+    private Dialog paymentMethodDialog;
+    private ImageButton paytm;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +78,29 @@ public class DeliveryActivity extends AppCompatActivity {
         fullAddress = findViewById(R.id.addresses_fullname);
         pincode = findViewById(R.id.pincode);
         continueBtn = findViewById(R.id.cart_continue_btn);
+
+        ///loading dialog
+
+        loadingDialog = new Dialog(DeliveryActivity.this);
+        loadingDialog.setContentView(R.layout.loading_progress_dialog);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.slider_backgeound));
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        ///loading dialog
+
+
+        ///loading dialog
+
+        paymentMethodDialog = new Dialog(DeliveryActivity.this);
+        paymentMethodDialog.setContentView(R.layout.payment_method);
+        paymentMethodDialog.setCancelable(true);
+        paymentMethodDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.slider_backgeound));
+        paymentMethodDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        paytm = paymentMethodDialog.findViewById(R.id.paytm);
+
+
+        ///loading dialog
 
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -71,10 +124,121 @@ public class DeliveryActivity extends AppCompatActivity {
         continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                paymentMethodDialog.show();
             }
         });
 
+        paytm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                paymentMethodDialog.dismiss();
+                loadingDialog.show();
+                if (ContextCompat.checkSelfPermission(DeliveryActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(DeliveryActivity.this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, 101);
+                }
+
+                final String M_id = "YlutwW8409962366406";
+                final String customer_id = FirebaseAuth.getInstance().getUid();
+                final String order_id = UUID.randomUUID().toString().substring(0,28);
+                String url = "https://hesperian-documents.000webhostapp.com/paytm/generatedChecksum.php";
+                final String callBackUrl = "https://pguat.patm.com/paytmchecksum/paytmCallback.jsp";
+
+                RequestQueue requestQueue = Volley.newRequestQueue(DeliveryActivity.this);
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.has("CHECKSUMHASH")){
+                                String CHECKSUMHASH = jsonObject.getString("CHECKSUMHASH");
+
+//                                PaytmPGService paytmPGService = PaytmPGService.getStagingService();
+                                PaytmPGService paytmPGService = PaytmPGService.getProductionService();
+                                HashMap<String, String> paramMap = new HashMap<String,String>();
+                                paramMap.put( "MID" , M_id);
+                                paramMap.put( "ORDER_ID" , order_id);
+                                paramMap.put( "CUST_ID" , customer_id);
+                                paramMap.put( "CHANNEL_ID" , "WAP");
+                                paramMap.put( "TXN_AMOUNT" , totalAmount.getText().toString().substring(3,totalAmount.getText().length()-2));
+                                paramMap.put( "WEBSITE" , "WEBSTAGING");
+                                paramMap.put( "INDUSTRY_TYPE_ID" , "Retail");
+                                paramMap.put( "CALLBACK_URL", callBackUrl);
+                                paramMap.put("CHECKSUMHASH", CHECKSUMHASH);
+
+                                PaytmOrder order = new PaytmOrder(paramMap);
+
+                                paytmPGService.initialize(order,null);
+                                paytmPGService.startPaymentTransaction(DeliveryActivity.this, true, true, new PaytmPaymentTransactionCallback() {
+                                    @Override
+                                    public void onTransactionResponse(Bundle inResponse) {
+                                        Toast.makeText(getApplicationContext(), "Payment Transaction response " + inResponse.toString(), Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void networkNotAvailable() {
+                                        Toast.makeText(getApplicationContext(), "Network connection error: Check your internet connectivity", Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void clientAuthenticationFailed(String inErrorMessage) {
+                                        Toast.makeText(getApplicationContext(), "Authentication failed: Server error" + inErrorMessage.toString(), Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void someUIErrorOccurred(String inErrorMessage) {
+                                        Toast.makeText(getApplicationContext(), "UI Error " + inErrorMessage , Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void onErrorLoadingWebPage(int iniErrorCode, String inErrorMessage, String inFailingUrl) {
+                                        Toast.makeText(getApplicationContext(), "Unable to load webpage " + inErrorMessage.toString(), Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void onBackPressedCancelTransaction() {
+                                        Toast.makeText(getApplicationContext(), "Transaction cancelled" , Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void onTransactionCancel(String inErrorMessage, Bundle inResponse) {
+                                        Toast.makeText(getApplicationContext(), "Transaction cancelled" , Toast.LENGTH_LONG).show();
+                                    }
+                                });
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loadingDialog.dismiss();
+                        Toast.makeText(DeliveryActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> paramMap = new HashMap<String,String>();
+                        paramMap.put( "MID" , M_id);
+                        paramMap.put( "ORDER_ID" , order_id);
+                        paramMap.put( "CUST_ID" , customer_id);
+                        paramMap.put( "CHANNEL_ID" , "WAP");
+                        paramMap.put( "TXN_AMOUNT" , totalAmount.getText().toString().substring(3,totalAmount.getText().length()-2));
+                        paramMap.put( "WEBSITE" , "WEBSTAGING");
+                        paramMap.put( "INDUSTRY_TYPE_ID" , "Retail");
+                        paramMap.put( "CALLBACK_URL", callBackUrl);
+                        return paramMap;
+                    }
+                };
+
+                requestQueue.add(stringRequest);
+
+            }
+        });
 
 
     }
@@ -98,5 +262,11 @@ public class DeliveryActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        loadingDialog.dismiss();
     }
 }
